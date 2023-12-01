eee07acbc9e47c19499806c562d5e1ee133f573b
Unfortunate3NS34RQU3 <logan.losio@ensea.fr>
2023-12-01 09:45:41 +0100

    TpShell_Q2

---
+++ TpShell_Q2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define TAILLE_BUF 100 // Taille attendue de l'entrée
+
+void main() {
+    char buf[TAILLE_BUF];
+    ssize_t readNb; //nombre de caractères lus
+
+    while (1) {
+        write(1, "enseash % ", 11); // Affiche le prompt
+        
+        readNb = read(0, buf, TAILLE_BUF); // Lit la commande de l'utilisateur
+        if (readNb == -1) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+
+        buf[readNb - 1] = '\0'; // Remplace le saut de ligne par le caractère de fin de chaîne
+
+        pid_t pid = fork(); // Crée un nouveau processus
+        if  (pid == 0) {
+            // Processus enfant
+            execlp(buf, buf, (char *)NULL); // Exécute la commande saisie
+            perror("execlp"); // Gère l'erreur si l'execution échoue
+            exit(EXIT_FAILURE); // Termine le processus enfant en cas d'échec
+        } else {
+            // Processus parent
+            waitpid(pid, NULL, 0); // Attend la fin du processus enfant 
+        }
+    }
+}
+
